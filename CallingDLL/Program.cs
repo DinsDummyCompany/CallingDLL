7540cf96ce17f442e6d266f57e76ce8b0ed3f6ba
Jonathan Carnie <jono.carnie@live.com.au>
2015-07-26 11:28:06 +1000

    Code running with format of input being checked for.

---
+++ CallingDLL/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using CommonFuncs;
+
+namespace CallingDLL
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args.Length != 2)
+            {
+                System.Console.WriteLine("Usage: CallingDLL <num1> <num2>");
+                return;
+            }
+
+            long num1, num2;
+            
+            try
+            {
+                num1 = long.Parse(args[0]);
+                num2 = long.Parse(args[1]);
+            }
+            catch (System.FormatException e)
+            {
+
+                Console.WriteLine("Format of input insn't valid, only integers allowed. {0}", e.Message);
+                return;
+            }
+
+
+
+            long sum = AddClass.Add(num1, num2);
+            long product = MultiplyClass.Multiply(num1, num2);
+           
+
+
+            float div = DivideClass.Divide(num1 / 1L, num2 / 1L);
+
+            System.Console.WriteLine("{0} + {1} = {2}", num1, num2, sum);
+            System.Console.WriteLine("{0} * {1} = {2}", num1, num2, product);
+            System.Console.WriteLine("{0} / {1} = {2}", num1, num2, div);
+
+
+
+        }
+
+
+
+
+    }
+}
---
+++ CommonFuncs/FuncCode.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CommonFuncs
+{
+    public class AddClass
+    {
+        public static long Add(long i, long j)
+        {
+            return i + j;
+        }
+    }
+
+    public class MultiplyClass
+    {
+
+        public static long Multiply(long x, long y)
+        {
+            return (x * y);
+        }
+        
+    }
+
+
+    public class DivideClass
+    {
+        public static float Divide(float a, float b)
+        {
+            return a / b;
+        }
+
+     }
+
+
+
+
+}
